754607df05c326783ecf62d2d77b7cce5a11b2f5
Alex <alex.beciana@gmail.com>
2020-07-20 14:32:11 -0400

    Modified trainer.rb

--- gym/app/models/location.rb
+++ gym/app/models/location.rb
@@ -32,3 +32,4 @@ class Location
     end
 
 end
+
--- gym/app/models/trainer.rb
+++ gym/app/models/trainer.rb
@@ -36,22 +36,7 @@ class Trainer
     def self.most_clients
         self.all.max do |a, b| 
             a.client_count <=> b.client_count
-            # binding.pry
         end
-        # .flatten[0]
-
-        # search = Gym.all.each_with_object(Hash.new(0)) do |gym, hash|
-        #     hash[gym.trainer.clients] += 1
-        # end 
-        # search
-        # binding.pry
-        # result = search.max { |a, b| a[1] <=> b[1]
-        # # binding.pry
-        
-        # # binding.pry
-        # val = result.trainer
-        # binding.pry
-        # return val[0].trainer
     end
 
     
